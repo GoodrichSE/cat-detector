da6f17989920cee5699daaf3417141385c351004
Spencer Goodrich <goodrichse@gmail.com>
2021-01-05 21:48:08 -0800

    Making the cpp binary output to the browser view instead of just the CLI

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,27 +1,27 @@
 const express = require('express')
-const app = express()
+const path = require('path')
 const port = 5000
 const { exec } = require('child_process');
+var app = express()
 
 const cat = exec('./../cpp/build/Main gettyimages-480868504-640_adpp.mp4', (err, stdout, stderr) => {
-	  if (err) {
-	    //some err occurred
-	    console.error(err)
-	  } else {
-	   // the *entire* stdout and stderr (buffered)
-	   console.log(`stdout: ${stdout}`);
-	   console.log(`stderr: ${stderr}`);
-	  }
-	});
+  console.log('Executable called.')
+  if (err) {
+    //some err occurred
+    console.log('Error occurred.')
+    console.error(err)
+  } else {
+   // the *entire* stdout and stderr (buffered)
+   console.log(`stdout: ${stdout}`);
+   console.log(`stderr: ${stderr}`);
+  }
+})
 
+app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.get('/cat', (req, res) => {
   res.send(cat())
 })
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-
-
+app.listen(port, () => console.log('Example app listening at http://localhost:${ port }'))
