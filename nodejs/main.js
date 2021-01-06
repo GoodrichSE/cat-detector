7f373daef85e1d4f82f7ca59bf3f7633e547844f
Spencer Goodrich <goodrichse@gmail.com>
2021-01-05 23:26:58 -0800

    Writing tests to learn how to call different commands through heroku/express

--- nodejs/main.js
+++ nodejs/main.js
@@ -4,6 +4,8 @@ const port = 5000
 const { exec } = require('child_process');
 var app = express()
 
+const dog = exec('echo "Arf"')
+
 const cat = exec('./../cpp/build/Main gettyimages-480868504-640_adpp.mp4', (err, stdout, stderr) => {
   console.log('Executable called.')
   if (err) {
@@ -20,6 +22,9 @@ const cat = exec('./../cpp/build/Main gettyimages-480868504-640_adpp.mp4', (err,
 app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+app.get('/dog', (req, res) => {
+  res.send(dog())
 })
 app.get('/cat', (req, res) => {
   res.send(cat())
