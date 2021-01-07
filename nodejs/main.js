1298d212786812d795ac5dc5167343c89a9ddde5
goodrichse <goodrichse@gmail.com>
2021-01-06 22:29:01 -0800

    Working on getting bash code to execute correctly

--- nodejs/main.js
+++ nodejs/main.js
@@ -4,50 +4,76 @@ const port = 5000
 const { exec } = require('child_process');
 var app = express()
 
+// These are not properly defined as functions
 // Make these callbacks instead, with function(){}
-var dog = exec('echo "Arf"')
+function dog () {
+	str = exec('echo "Arf"')
+	return "Arf!" 
+}
 
-var pig = exec('./../cpp/build/Hello')
+function pig () {
+	exec('./../cpp/build/Hello')
+}
 
-var cat = exec('./../cpp/build/Main gettyimages-480868504-640_adpp.mp4', (err, stdout, stderr) => {
-  console.log('Executable called.')
-  if (err) {
-    //some err occurred
-    console.log('Error occurred.')
-    console.error(err)
-  } else {
-   // the *entire* stdout and stderr (buffered)
-   console.log(`stdout: ${stdout}`);
-   console.log(`stderr: ${stderr}`);
-  }
-})
+function cat () {
+	exec('./../cpp/build/Main gettyimages-480868504-640_adpp.mp4', (err, stdout, stderr) => {
+	  console.log('Executable called.')
+	  if (err) {
+	    //some err occurred
+	    console.log('Error occurred.')
+	    console.error(err)
+	  } else {
+	   // the *entire* stdout and stderr (buffered)
+	   console.log(`stdout: ${stdout}`);
+	   console.log(`stderr: ${stderr}`);
+	  }
+	})
+}
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.get('/dog', (req, res) => {
-  res.send(dog())
-})
-app.get('/pig', (req, res) => {
-  res.send(pig())
-})
-app.get('/cat', (req, res) => {
-  res.send(cat())
-})
-app.get('/bird', (req, res) => {
-  res.send(exec('./../cpp/build/Main gettyimages-480868504-640_adpp.mp4', (err, stdout, stderr) => {
-    console.log('Executable called.')
-    if (err) {
-      //some err occurred
-      console.log('Error occurred.')
-      console.error(err)
-    } else {
-     // the *entire* stdout and stderr (buffered)
-     console.log(`stdout: ${stdout}`);
-     console.log(`stderr: ${stderr}`);
-    }
-  })
-  )
-})
-app.listen(port, () => console.log('Example app listening at http://localhost:${ port }'))
+
+function test_console () {
+	console.log(dog())
+	console.log(pig())
+	console.log(cat())
+}
+
+function test_response () {
+	// Direct string response
+	app.get('/', (req, res) => {
+	  res.send('Hello World!')
+	})
+	// String response called through bash on the server
+	app.get('/dog', (req, res) => {
+	  res.send(dog())
+	})
+	// String response called through bash on the server which calls a simple cpp binary
+	app.get('/pig', (req, res) => {
+	  res.send(pig())
+	})
+	// String response called through bash on the server which calls a more complex cpp binary
+	app.get('/cat', (req, res) => {
+	  res.send(cat())
+	})
+	// Inline call of the complex cpp binary via bash
+	// This returns what seems to be an "exec" initiated with the bash command, instead of the results of the command
+	app.get('/bird', (req, res) => {
+	  res.send(exec('./../cpp/build/Main ../gettyimages-480868504-640_adpp.mp4', (err, stdout, stderr) => {
+	    console.log('Executable called.')
+	    if (err) {
+	      //some err occurred
+	      console.log('Error occurred.')
+	      console.error(err)
+	    } else {
+	     // the *entire* stdout and stderr (buffered)
+	     console.log(`stdout: ${stdout}`);
+	     console.log(`stderr: ${stderr}`);
+	    }
+	  })
+	  )
+	})
+	app.listen(port, () => console.log(`Example app listening at http://localhost:${ port }`))
+}
+
+test_console()
+//test_response()
